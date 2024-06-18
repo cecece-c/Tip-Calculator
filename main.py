9bd93b5c4370bc014a2a65295ac7a20a1abc58b8
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-19 00:27:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import math, time
 
 
-# Display banner
+# Display welcome message
 print("\nWelcome to the Tip Calculator.")
 
 
-# Get total bill, tip percentage and number of people, and store values in 'total_bill' (Float), 'tip_percentage' (String) and 'number_of_people' (Float) respectively
+# Get total bill, tip percentage and number of people, and store it in 'total_bill' (Float), 'tip_percentage' (String) and 'number_of_people' (Float) respectively
 while True:
     try:
         total_bill = float(input("\nWhat was the total bill?\n$"))
@@ -18,42 +18,45 @@ while True:
                 if number_of_people >= 1:
                     break
                 else:
-                    print("\nInvalid input. Only numbers above 1 are accepted.")
+                    print("\nInvalid input. Only numbers above or equal to 1 are accepted.")
             elif tip_percentage == "10%" or tip_percentage == "10":
                 tip_percentage = 0.10
                 number_of_people = math.ceil(float(input("\nHow many people will the bill be split between?\n")))
                 if number_of_people >= 1:
                     break
                 else:
-                    print("\nInvalid input. Only numbers above 1 are accepted.")
+                    print("\nInvalid input. Only numbers above or equal to 1 are accepted.")
             elif tip_percentage == "15%" or tip_percentage == "15":
                 tip_percentage = 0.15
                 number_of_people = math.ceil(float(input("\nHow many people will the bill be split between?\n")))
                 if number_of_people >= 1:
                     break
                 else:
-                    print("\nInvalid input. Only numbers above 1 are accepted.")
+                    print("\nInvalid input. Only numbers above or equal to 1 are accepted.")
             else:
-                print("\nInvalid input. Only listed percentages are accepted.")
+                print("\nInvalid input. Only listed options are accepted.")
         else:
             print("\nInvalid input. Only numbers above 0 are accepted.")
     except ValueError:
         print("\nInvalid input. Only numbers are accepted.")
 
 
-# Calculate new total bill after adding tip and store value in 'total_bill' (Float)
+# Calculate new total bill after adding tip and store it in 'total_bill' (Float)
 total_bill += total_bill * tip_percentage
 
 
-# Calculate amount payable per person and store value in 'payable_per_person' (Float)
+# Calculate amount payable per person, round it to 2 decimal places, and store it in 'payable_per_person' (Float)
 payable_per_person = round(total_bill / number_of_people, 2)
 
 
-# Diplay amount payable per person
+# Diplay amount to be paid per person
 print(f"\nEach person should pay:\n${payable_per_person:.2f}")
 
 
-# Exit program
+# Display exit message
 print("\nProgram exiting...")
+
+
+# Exit program
 for delay in range(5):
     time.sleep(1)
